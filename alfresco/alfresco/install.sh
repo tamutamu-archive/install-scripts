ec4c7a7ae89b8d9eed2462d5de9ca4313f1bdfdb
root <root@localhost.localdomain>
2017-07-24 15:30:31 +0900

    test

--- alfresco/alfresco/install.sh
+++ alfresco/alfresco/install.sh
@@ -9,7 +9,7 @@ declare -r CUR=$(cd $(dirname $0); pwd)
 pushd ${CUR}
 
 
-### Install mysql-5.7.
+#### Install mysql-5.7.
 pushd ../../mysql/mysql-5.7/
 ./install.sh
 
@@ -18,7 +18,16 @@ pushd ../../mysql/mysql-5.7/
 popd
 
 
-### Install.
+### Create Alfresco Database.
+sed -e "s/#ALFRESCO_DB_NAME#/${ALFRESCO_DB_NAME}/" \
+    -e "s/#ALFRESCO_DB_USER#/${ALFRESCO_DB_USER}/" \
+    -e "s/#ALFRESCO_DB_PASS#/${ALFRESCO_DB_PASS}/" \
+  ${CUR}/conf/create_database.sh.tmpl > ./create_database.sh
+
+mysql -uroot -p${MYSQL_ROOT_PASS} < ./create_database.sh
+
+
+### Install Alfresco.
 pushd /tmp
 
 # Get Installer file.
@@ -29,21 +38,19 @@ chmod u+x ./${installer_name}
 # Prepare install option file.
 sed -e "s/#ALFRESCO_ADMIN_PASS#/${ALFRESCO_ADMIN_PASS}/" ${CUR}/conf/optionfile.tmpl > ./optionfile
 
-
-# Create Database.
-sed -e "s/#ALFRESCO_DB_NAME#/${ALFRESCO_DB_NAME}/" \
-    -e "s/#ALFRESCO_DB_USER#/${ALFRESCO_DB_USER}/" \
-    -e "s/#ALFRESCO_DB_PASS#/${ALFRESCO_DB_PASS}/" \
-  ${CUR}/conf/create_database.sh.tmpl > ./create_database.sh
-
-mysql -uroot -p${MYSQL_ROOT_PASS} < ./create_database.sh
+# Install.
+./${installer_name}  --optionfile ./optionfile
 
 # Put JDBC Driver.
 pushd /tmp
-curl -O https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.43.tar.gz
+curl -O -L https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.43.tar.gz
 tar -xzvf mysql-connector-java-5.1.43.tar.gz
-cp mysql-connector-java-5.1.43-bin.jar /opt/alfresco/tomcat/lib/
+cp ./mysql-connector-java-5.1.43/mysql-connector-java-5.1.43-bin.jar /opt/alfresco/tomcat/lib/
 popd
+
+
+### Starting...
+service alfresco start
 
 
 ### End
